09a2d34d97e37843793f27ee6f9921cd55b03a85
KirthiVignesh <kirthivignesh.kvg@protonmail.com>
2023-04-25 22:11:42 +0530

    added health integration template

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,3 +1,4 @@
+// import 'package:health/health.dart';
 import 'package:nimmadhi/pages/profile/create_profile.dart';
 import 'package:nimmadhi/utils/get_scores.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -24,6 +25,45 @@ class _ProfilePageState extends State<ProfilePage> {
 
   final user = FirebaseAuth.instance.currentUser!;
   Map<String, dynamic>? _userDetails;
+
+  // void HealthDataExample() async {
+  //   // create a HealthFactory for use in the app
+  //   HealthFactory health = HealthFactory();
+
+  //   // define the types to get
+  //   var types = [
+  //     HealthDataType.STEPS,
+  //     HealthDataType.BLOOD_GLUCOSE,
+  //   ];
+
+  //   // requesting access to the data types before reading them
+  //   bool requested = await health.requestAuthorization(types);
+
+  //   var now = DateTime.now();
+
+  //   // fetch health data from the last 24 hours
+  //   List<HealthDataPoint> healthData = await health.getHealthDataFromTypes(
+  //       now.subtract(Duration(days: 1)), now, types);
+
+  //   // request permissions to write steps and blood glucose
+  //   types = [HealthDataType.STEPS, HealthDataType.BLOOD_GLUCOSE];
+  //   var permissions = [
+  //     HealthDataAccess.READ_WRITE,
+  //     HealthDataAccess.READ_WRITE
+  //   ];
+  //   await health.requestAuthorization(types, permissions: permissions);
+
+  //   // write steps and blood glucose
+  //   bool success =
+  //       await health.writeHealthData(10, HealthDataType.STEPS, now, now);
+  //   success = await health.writeHealthData(
+  //       3.1, HealthDataType.BLOOD_GLUCOSE, now, now);
+
+  //   // get the number of steps for today
+  //   var midnight = DateTime(now.year, now.month, now.day);
+  //   int? steps = await health.getTotalStepsInInterval(midnight, now);
+  // }
+
   Future<void> _getUserDetails() async {
     FirebaseFirestore.instance
         .collection('users')
@@ -177,13 +217,25 @@ class _ProfilePageState extends State<ProfilePage> {
           bottom: 25,
           right: 25,
           child: FloatingActionButton(
-            backgroundColor: Colors.white,
-            foregroundColor: Colors.indigo,
+            // backgroundColor: Colors.white,
+            // foregroundColor: Colors.indigo,
             onPressed: () {
               Navigator.of(context).push(
                   MaterialPageRoute(builder: (context) => CreateProfile()));
             },
             child: Icon(FlutterRemix.pencil_line),
+          ),
+        ),
+        Positioned(
+          bottom: 25,
+          left: 25,
+          child: FloatingActionButton(
+            // backgroundColor: Colors.white,
+            // foregroundColor: Colors.indigo,
+            onPressed: () {
+              // HealthDataExample;
+            },
+            child: Icon(FlutterRemix.heart_2_line),
           ),
         ),
       ]),
